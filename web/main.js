ca89d965f2da783119ab745fb7dad435b1d0a59a
Adrien Husson <pangel.neu@gmail.com>
2018-02-14 18:29:37 +0100

    More node colors. Fixes [mapping] bug.

--- web/main.js
+++ web/main.js
@@ -1,6 +1,7 @@
 let cytoscape = require('./vendor/cytoscape');
 let graph = require('./lib/graph');
 let domtoimage = require('./vendor/dom-to-image');
+let colors = require('./lib/colors')();
 
 let WS = require('./lib/ws-client');
 
@@ -54,29 +55,6 @@ ws_client.connect();
 cytoscape.use(require('./vendor/cytoscape-dagre'));
 cytoscape.use(require('./vendor/cytoscape-cose-bilkent'));
 
-window.kelly_colors_hex = [
-    "#FFB300", // Vivid Yellow
-    "#803E75", // Strong Purple
-    "#FF6800", // Vivid Orange
-    "#A6BDD7", // Very Light Blue
-    "#C10020", // Vivid Red
-    "#CEA262", // Grayish Yellow
-    "#817066", // Medium Gray
-    "#007D34", // Vivid Green
-    "#F6768E", // Strong Purplish Pink
-    "#00538A", // Strong Blue
-    "#FF7A5C", // Strong Yellowish Pink
-    "#53377A", // Strong Violet
-    "#FF8E00", // Vivid Orange Yellow
-    "#B32851", // Strong Purplish Red
-    "#F4C800", // Vivid Greenish Yellow
-    "#7F180D", // Strong Reddish Brown
-    "#93AA00", // Vivid Yellowish Green
-    "#593315", // Deep Yellowish Brown
-    "#F13A13", // Vivid Reddish Orange
-    "#232C16", // Dark Olive Green
-    ];
-
 let init = (cyd_basis,cyd_graphs) => {
   clear();
 
@@ -110,7 +88,7 @@ let init = (cyd_basis,cyd_graphs) => {
         {
           selector: 'node[highlight]',
           style: {
-            'background-color':function(e) { return kelly_colors_hex[e.data("highlight")] },
+            'background-color': e => colors.get(e.data("highlight")),
           }
         },
         {
